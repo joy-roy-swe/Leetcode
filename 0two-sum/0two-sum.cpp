323525849a36dd3ae97e963a0fbf5a008327ea59
Joy Roy <43453620+joy-roy-swe@users.noreply.github.com>
2023-11-11 00:11:02 +0600

    Update two-sum.cpp

--- 0two-sum/0two-sum.cpp
+++ 0two-sum/0two-sum.cpp
@@ -7,6 +7,7 @@ public:
             if(map.find(another) != map.end()){
                 return {map[another], i};
             }
+            # here, add the key-value pair (vector value, vector index) to the map. The map now looks like this: {vector value : vector index}
             map[nums[i]] = i;
         }
         return {-1, -1};
