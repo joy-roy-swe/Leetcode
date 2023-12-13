d0e371d995aa32776048b442287f861ec2475822
Codebind001 <joy.roy.nil76@gmail.com>
2023-12-14 00:22:04 +0600

    Time 21 ms, Memory: 13.1 MB

--- special_positions_in_a_binary_matrix/special_positions_in_a_binary_matrix.cpp
+++ special_positions_in_a_binary_matrix/special_positions_in_a_binary_matrix.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        // Brute Force Solution
-        bool find = true;
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<int> rowCount(m, 0); // Count of '1's in each row
+        vector<int> colCount(n, 0); // Count of '1's in each column
         int special = 0;
-        for(int i=0; i<mat.size(); ++i){
-            for(int j=0; j<mat[0].size(); ++j){
-                // If the element is 0, continue to the next iteration
-                if(mat[i][j] == 0)
-                    continue;
-                // Flag to determine if the element is "special"
-                find = true;
-                // Checking the entire column for other '1's except for the current row
-                for(int row=0; row<mat.size(); ++row){
-                    if(row!=i && mat[row][j]==1){
-                        // If a '1' is found in the same column in another row, mark as not "special"
-                        find = false; 
-                        break;
-                    }
-                }
-                // Checking the entire row for other '1's except for the current column
-                for(int col=0; col<mat[0].size(); ++col){
-                    if(col!=j && mat[i][col]==1){
-                        // If a '1' is found in the same row in another column, mark as not "special"
-                        find = false; 
-                        break;
-                    }
-                }
-                // If the element is "special," increment the count
-                if(find) 
-                    special++;
-            } 
+
+        // Calculate counts of '1's in each row and each column
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                rowCount[i] += mat[i][j];
+                colCount[j] += mat[i][j];
+            }
         }
-        // Return the count of "special" elements
+
+        // Check if each '1' is the only '1' in its row and column
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+                    special++;
+                }
+            }
+        }
+
         return special;
     }
 };
