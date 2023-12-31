807c81f784b8facbc2bc47472988a85078ed74e6
Codebind001 <joy.roy.nil76@gmail.com>
2023-12-31 21:42:44 +0600

    Time 0 ms, Memory: 6.71 MB

--- largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
+        // Variable to store the maximum distance between equal characters
         int max_length = -1;
-        for(int i=0; i<s.size(); ++i){
-            for(int j=i+1; j<s.size(); ++j){
-                if(s[i]==s[j]){
-                    max_length = max(max_length, j-i-1);
-                }
+        
+        // Map to store the last index where each character was encountered
+        unordered_map<char, int> characters;
+        
+        // Iterate through the string
+        for (int i = 0; i < s.size(); ++i) {
+            // Check if the current character has been encountered before
+            if (characters.find(s[i]) != characters.end()) {
+                // Calculate the distance between the current index and the last index of the character
+                max_length = max(max_length, i - characters[s[i]] - 1);
+            } else {
+                // Update the map with the current character and its index
+                characters[s[i]] = i;
             }
-        } 
+        }
+        
+        // Return the maximum distance between equal characters
         return max_length;
     }
 };
