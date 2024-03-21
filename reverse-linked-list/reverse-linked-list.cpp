1dc37e81cd33cb7c47ab12097a49c2ac11b81fa5
Codebind001 <joy.roy.nil76@gmail.com>
2024-03-21 23:30:38 +0600

    Time 10 ms, Memory: 8.3 MB - CPP

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -1,20 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* current = head;
-        ListNode *prev = NULL, *next = NULL;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
 
-        while (current != NULL) {
+        while (current != nullptr) {
             next = current->next;    // Save the next node
             current->next = prev;    // Reverse the link
             prev = current;          // Move prev to the current node
@@ -24,3 +28,42 @@ public:
         return head;
     }
 };
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Print original list
+    std::cout << "Original list: ";
+    printList(head);
+
+    // Reverse the list
+    Solution solution;
+    head = solution.reverseList(head);
+
+    // Print reversed list
+    std::cout << "Reversed list: ";
+    printList(head);
+
+    // Clean up
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
